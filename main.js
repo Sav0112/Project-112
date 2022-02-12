52faf678028414e339c01cae16313308140bd0f4
Sav0112 <74131851+Sav0112@users.noreply.github.com>
2022-02-12 15:47:28 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -12,8 +12,8 @@ camera = document.getElementById("camera");
 Webcam.attach('#camera');
 
 function take_snapshot() {
-    webcam.snap(function(data_uri){
-        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'">';
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
     });
 
 }
